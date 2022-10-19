3b4cf8fa7cca20732087001514d63f94c367b2c6
keith bova <keithhbova@gmail.com>
2022-10-19 11:27:15 -0600

    changed dialog name

--- dcmToJpeg.py
+++ dcmToJpeg.py
@@ -108,7 +108,7 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        Dialog.setWindowTitle(_translate("Dialog", "dicom to jpeg converter"))
         self.pushButton.setText(_translate("Dialog", "Search"))
         self.label_2.setText(_translate("Dialog", "Directory:"))
 
